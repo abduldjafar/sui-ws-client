32b90409820e8ce5bdfb1dc74743d74ae32d8f7f
abdul-haris-djafar <abdul.djafar@autonomic.ai>
2022-09-20 13:54:04 +0700

    add integration with clickhouse

--- core/loadto/clickhouse.py
+++ core/loadto/clickhouse.py
@@ -14,7 +14,6 @@ class ClickhouseServices(object):
         self.db_name = "sui_dev"
         self.tb_name = "raw_transactions"
         self.columns = "(raw_datas)"
-        self.datas = []
 
     def create_sui_dev_database(self):
         create_database_query = self.query.create_database(self.db_name)
@@ -29,14 +28,15 @@ class ClickhouseServices(object):
 
 
     def insert_into_table(self, message):
-        self.datas.append({ "raw_datas" : str(message.data).replace("\\","") })
+        datas = [{ "raw_datas" : str(message.data).replace("\\","") }]
         insert_datas_query = self.query.insert_data_to_table(self.db_name,self.tb_name,self.columns)
 
-        if len(self.datas) == 100:
-            err_insert_datas, message_insert_datas =  self.clickhouse.insert_data(insert_datas_query,self.datas)
-            logging.info(message_insert_datas)
-            self.datas = []
+       
+        err_insert_datas, message_insert_datas =  self.clickhouse.insert_data(insert_datas_query,datas)
+        logging.info(message_insert_datas)
+
         sleep(1)
+
         message.ack()
 
         
