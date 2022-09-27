cfef09b215bdc54d9b200292a2a1cccd3a43e3f3
abdul-haris-djafar <abdul.djafar@autonomic.ai>
2022-09-27 08:30:51 +0700

    add integration with rocketset

--- main.py
+++ main.py
@@ -41,6 +41,8 @@ def batch_to_rocketset(url, batch_size=1000, filepath="output.log", inc=1000):
         datas = jrpc_services.get_transactions_range(start, end)
         datas_result = jrpc_services.jrpc_post(datas["transactions_id"])
 
+        print(datas_result)
+
         if len(datas_result) > 0:
             datas_result = [
                 {
